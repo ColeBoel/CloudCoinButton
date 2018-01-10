8007b408d9b0dab41acb25bf360541215ab6aec2
ColeBoel <joshcboelman@gmail.com>
2018-01-10 13:10:02 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+app.on('ready',() => {
+  let win = new BrowserWindow({width: 800,height: 600})
+  win.loadURL(`file://${__dirname}/index.html`)
+})
+
+$(app).on('change', '.btn-file :file', function() {
+  var input = $(this),
+      numFiles = input.get(0).files ? input.get(0).files.length : 1,
+      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+  input.trigger('fileselect', [numFiles, label]);
+});
+
+$(app).ready( function() {
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        if( input.length ) {
+            input.val(log);
+        } else {
+            if( log ) alert(log);
+        }
+
+    });
+});
